110fb5c06304574574a97879af2eab9c6ff17e01
Joachim Kainz <jfk@jolira.com>
2012-04-14 16:46:24 -0700

    upgraded to latest versions.

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,28 @@
 /*global PhoneGap:false, require:false */
 require.config({
     paths: {
-        jquery: 'libs/zepto-0.8',
-        underscore: 'libs/underscore/underscore-1.3.1-amdjs',
-        backbone: 'libs/backbone/backbone-0.9.1-amdjs'
+        jquery: 'libs/zepto-1.0RC1',
+        underscore: 'libs/underscore/underscore-1.3.2-amdjs',
+        backbone: 'libs/backbone/backbone-0.9.2-amdjs'
     }
 });
 
 require(["underscore", "backbone", "jquery", "project"], function (_, Backbone, $, project) {
     "use strict";
 
+        var Router = Backbone.Router.extend({
+            routes: {
+                "nodes/:project":          "renderProject",
+                "nodes/:project/:release": "renderProject",
+                "*actions":                "renderProject"
+            },
+            renderProject: function(){
+                project.apply(this, arguments);
+            }
+        });
+
+        new Router();
+        Backbone.history.start({pushState: true});
     $(function() {
-        project();
     });
 });
--- public/js/project.js
+++ public/js/project.js
@@ -1,20 +1,6 @@
 define(["underscore", "backbone", "jquery"], function (_, Backbone, $) {
-    // Model
-    // ----------
-    var Project = Backbone.Model.extend({
-        // read-only
-    });
-
-    // Collection
-    // ---------------
-    var ProjectList = Backbone.Collection.extend({
-        model:Project,
-        url:'/projects'
-    });
-
-    // Project View
-    // --------------
-    var ProjectView = Backbone.View.extend({
+    var Release = Backbone.Model.extend({});
+    var ReleaseView = Backbone.View.extend({
         tagName:"li",
         template:_.template($('#project-template').html()),
         events:{
@@ -25,29 +11,64 @@ define(["underscore", "backbone", "jquery"], function (_, Backbone, $) {
             this.$el.toggleClass('done', this.model.get('done'));
             this.input = this.$('.edit');
             return this;
+        },
+        viewNotes:function(){
+
         }
     });
-
-    // Project List View
-    // --------------
-    var ProjectListView = Backbone.View.extend({
+    var ReleaseList = Backbone.Collection.extend({
+        model:Release,
+        url:'/releases'
+    });
+    var ReleaseListView = Backbone.View.extend({
         initialize:function () {
             this.model.bind('reset', this.addAll, this);
             this.model.fetch();
         },
-
-        addOne:function (project) {
-            var view = new ProjectView({model:project});
-            this.$("#todo-list").append(view.render().el);
-        },
-
-        // Add all items in the **Todos** collection at once.
         addAll:function () {
-            this.model.each(this.addOne);
+            this.model.each(function(release){
+                var view = new ReleaseView({model:project});
+                this.$("#release-list").append(view.render().el);
+            });
+        }
+    });
+
+    var Project = Backbone.Model.extend({
+        // read-only
+    });
+
+    var ProjectView = Backbone.View.extend({
+        tagName:"option",
+        render:function () {
+            var vals = this.model.toJSON();
+
+            this.$el.value = vals.id;
+            this.$el.html(vals.name);
+
+            return this;
+        }
+    });
+
+    var ProjectList = Backbone.Collection.extend({
+        model:Project,
+        url:'/projects'
+    });
+
+    var ProjectListView = Backbone.View.extend({
+        tagName:"select",
+        initialize:function () {
+            this.model.bind('reset', this.addAll, this);
+            this.model.fetch();
+        },
+        addAll:function () {
+            this.model.each(function(project){
+                var view = new ProjectView({model:project});
+                this.$el.append(view.render().el);
+            });
         }
     });
 
     return function () {
-        return new ProjectListView({ model:new ProjectList() })
+        return new ReleaseListView({ model:new ReleaseList() })
     };
 });
