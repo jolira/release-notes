d463efb164dc914e4690db0c40acb68eaaf0ab23
Joachim Kainz <jfk@jolira.com>
2012-04-20 13:01:39 -0700

    using new properties mechanism

--- index.js
+++ index.js
@@ -9,40 +9,44 @@ module.exports = require("./lib/build-report.js");
     }
 
     var mingle = require("./lib/mingle"),
-        mingleClient = mingle(process.env.MINGLE_URL, process.env.MINGLE_USERNAME, process.env.MINGLE_PASSWORD),
         projects = require("./lib/projects"),
         path = require("path"),
         templates = path.join(__dirname, "templates"),
         pubdir = path.join(__dirname, "public");
 
-    module.exports = {
-        "hostname": "release-notes.jolira.com",
-        "public": pubdir,
-        "googleAnalyticsWebPropertyID": "UA-3602945-1",
-        "title": "Release Notes",
-        "stylesheets": [
+    module.exports = function (defaults, cb, options) {
+        var mingleClient = mingle(process.env.MINGLE_URL, process.env.MINGLE_USERNAME, process.env.MINGLE_PASSWORD);
+
+        defaults.hostname = "release-notes.jolira.com";
+        defaults["public"] = pubdir,
+        defaults.googleAnalyticsWebPropertyID = "UA-3602945-1";
+        defaults.title = "Release Notes";
+        defaults.stylesheets = [
             "css/style.css",
             "css/release-notes.less"
-        ],
-        "metas": [{
-            "name": "description",
-            "content": "Release Notes from Jenkins and Mingle"
-        },
+        ];
+        defaults.metas = [
             {
-                "viewport" : "width=device-width,user-scalable=no,initial-scale=1"
+                "name":"description",
+                "content":"Release Notes from Jenkins and Mingle"
+            },
+            {
+                "viewport":"width=device-width,user-scalable=no,initial-scale=1"
             }
-        ],
-        "templateFiles": [
+        ];
+        defaults.templateFiles = [
             path.join(templates, "project-template.html")
-        ],
-        "htmlFiles": [
+        ];
+        defaults.htmlFiles = [
             path.join(templates, "main.html")
-        ],
-        scripts: [
+        ];
+        defaults.scripts = [
             "js/libs/modernizr-2.5.2.min.js"
-        ],
-        "dispatcher": {
-            projects: projects(mingleClient)
-        }
+        ];
+        defaults.dispatcher = {
+            projects:projects(mingleClient)
+        };
+
+        return cb(undefined, defaults);
     };
 })(module);
