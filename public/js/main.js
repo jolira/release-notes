4ee0b896db1ca43aa54bfc441af0d2697b81b805
Joachim Kainz <jfk@jolira.com>
2012-04-25 23:09:49 -0700

    using jquery-1.7.2

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 /*global PhoneGap:false, require:false */
 require.config({
     paths:{
-        jquery:'libs/zepto-1.0RC1',
+        jquery:'libs/jquery/jquery-1.7.2',
         underscore:'libs/underscore/underscore-1.3.2-amdjs',
         backbone:'libs/backbone/backbone-0.9.2-amdjs'
     }
